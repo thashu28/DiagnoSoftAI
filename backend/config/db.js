4b591f325d2c06890fc018984dbebe38d43ce41c
thashu28 <thashmithabs@gmail.com>
2024-10-12 01:10:33 -0400

    modified:   backend/.env 	modified:   backend/config/db.js 	modified:   frontend/src/App.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb://localhost:27017/diagnosoftai', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('MongoDB connection failed', err);
+    process.exit(1); // Stop the app if connection fails
   }
 };
 module.exports = connectDB;
