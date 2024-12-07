4ab81c247005a843bdf111ede7f18a3848fb07a8
Rishitha <rishiguttapalli2002@gmail.com>
2024-12-06 21:07:42 -0500

    deleted:    frontend/src/components/patients/patient_chat.jsx 	modified:   frontend/src/components/patients/patients_dashboard.jsx 	modified:   frontend/src/layout_routes/layout.jsx

--- frontend/src/layout_routes/layout.jsx
+++ frontend/src/layout_routes/layout.jsx
@@ -21,7 +21,7 @@ import DoctorProfile from '../components/doctors/doctor_profile'
 import ScheduleAppointments from '../components/patients/schedule_appointments';
 import PatientViewScans from '../components/patients/view_scans'; // New import
 import ViewReports from '../components/patients/view_reports'; // New import 
-import PatientChat from '../components/patients/patient_Chat';
+
 
 // Importing the new page in the Lab technician Dashboard
 import LabTechnicianChat from '../components/lab_technician/labtechnician_chat';
@@ -60,7 +60,7 @@ const Layout = () => {
           <Route path="/patients_dashboard/schedule_appointments" element={<ScheduleAppointments />} />
           <Route path="/patients_dashboard/view_scans" element={<PatientViewScans />} /> {/* New route */}
           <Route path="/patients_dashboard/view_reports" element={<ViewReports />} />
-          <Route path="/patients_dashboard/patient_chat" element={<PatientChat />} />
+          
           <Route path="/patients_dashboard/profile" element={<PatientProfile/>}/>
 
           {/* Lab Technician Dashboard Feature Routes */}
